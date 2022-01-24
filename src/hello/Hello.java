011e056f408e9eeb25fa4b661e65088c9376eeed
Ramia <ramiaarnouk@yahoo.com>
2022-01-24 10:39:32 -0500

    practice continue

--- src/hello/Hello.java
+++ src/hello/Hello.java
@@ -4,5 +4,6 @@ public class Hello {
     public static void main(String[] args) {
         //Hello World
         System.out.println("Hello world");
+        System.out.println("push your local to global");
     }
 }
